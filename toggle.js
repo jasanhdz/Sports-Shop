2fdcb8b8339d30d709a03104ae9f53922e7be6f8
Jasan Hernandez <jasan_azael@hotmail.es>
2019-02-16 19:35:59 -0600

    Agregando belazy.js

--- toggle.js
+++ toggle.js
@@ -13,10 +13,20 @@ const $burguerButton = document.getElementById('burguer-button');
       if(consulta.matches) {
         console.log("Se cumplió la condición");
         // Si la condición de la resolución se cumple activamos la clase css
-        $burguerButton.addEventListener('click', ToggleMenu);
+        $burguerButton.addEventListener('touchstart', ToggleMenu);
 
       } else {
-        $burguerButton.removeEventListener('click', ToggleMenu);
+        $burguerButton.removeEventListener('touchstart', ToggleMenu);
         console.log("No se cumplió la condición");
       }
     }
+    //Gestos Touch
+			var $body = document.body;
+			var gestos = new Hammer($body);
+			gestos.on('swipeleft', hideMenu);
+			gestos.on('swiperight', showMenu);
+
+    var bLazy = new Blazy({
+      // options
+      selector: 'img'
+  });
